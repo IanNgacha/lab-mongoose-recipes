f536faddde4365ddeed10f8ed44ea66b16359cec
IanNgacha <ngachaduncan53@gmail.com>
2024-08-08 14:55:51 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 const mongoose = require('mongoose');
-
-// Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
+const recipes = require('./data.json');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
-// Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
+// Connection to the database
+mongoose.connect('mongodb://localhost:27017/recipe-app', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    console.log('Connected to the database');
+
+    // Insert multiple recipes from data.json
+    return Recipe.insertMany(recipes);
+  })
+  .then(insertedRecipes => {
+    // Log the title of each inserted recipe
+    insertedRecipes.forEach(recipe => {
+      console.log('Recipe created:', recipe.title);
+    });
+
+    // Update the duration of "Rigatoni alla Genovese"
+    return Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese' },  
+      { duration: 100 },                   
+      { new: true }                        
+    );
+  })
+  .then(updatedRecipe => {
+    if (updatedRecipe) {
+      console.log(`Success! The duration of "${updatedRecipe.title}" has been updated to ${updatedRecipe.duration} minutes.`);
+    } else {
+      console.log('Recipe not found.');
+    }
+
+    // Delete the "Carrot Cake" recipe
+    return Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+  .then(deletionInfo => {
+    if (deletionInfo.deletedCount > 0) {
+      console.log('Success! The "Carrot Cake" recipe has been removed from the database.');
+    } else {
+      console.log('The "Carrot Cake" recipe was not found in the database.');
+    }
+
+    // Disconnect from the database after all operations
+    return mongoose.connection.close();
   })
   .catch(error => {
-    console.error('Error connecting to the database', error);
+    console.error('Error during database operations:', error);
   });
